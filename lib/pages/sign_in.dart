807872bbb553179362d5e8a5b74bf179ed9389d0
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-10 15:15:12 +0300

    initial commit

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -10,32 +10,46 @@ class SignIn extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-          backgroundColor2,
-          backgroundColor2,
-          backgroundColor4,
-        ])),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              backgroundColor2,
+              backgroundColor2,
+              backgroundColor4,
+            ],
+          ),
+        ),
         child: SafeArea(
-          child: ListView(
-            children: [
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              Text(
-                'Hello Again!',
-                style: TextStyle(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                Text(
+                  'Hello Again!',
+                  style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 37,
-                    color: textColor1),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Text(
-                "Welcome back! you've \nbeen missed!",
-                style: TextStyle(fontSize: 27, height: 1.2, color: textColor2),
-              ),
-            ],
+                    color: textColor1,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "Welcome back! you've \nbeen missed!",
+                  style: TextStyle(
+                    fontSize: 27,
+                    height: 1.2,
+                    color: textColor2,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ),
