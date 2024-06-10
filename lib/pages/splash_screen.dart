b4fb7bdbd640ad9fd6a2d5d9cfa0aed632ee98d5
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-10 14:58:06 +0300

    initial commit

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_page_flutter/pages/sign_in.dart';
 import 'package:login_page_flutter/utils/colors.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -29,60 +30,100 @@ class SplashScreen extends StatelessWidget {
               ),
             ),
             Positioned(
-                top: size.height * 0.6,
-                left: 0,
-                right: 0,
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        'Discover Your \nDream Job!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40,
-                            color: textColor1,
-                            height: 1.2),
+              top: size.height * 0.6,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Discover Your \nDream Job!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: textColor1,
+                          height: 1.2),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      'Explore the most exciting jobs roles\n based on your interest and ares of study',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        //fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: textColor2,
                       ),
-                      const SizedBox(
-                        height: 20,
+                    ),
+                    SizedBox(height: size.height * 0.07),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 30,
                       ),
-                      Text(
-                        'Explore the most exciting jobs roles\n based on your interest and ares of study',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: textColor2,
+                      child: Container(
+                        height: size.height * 0.08,
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: backgroundColor3.withOpacity(0.9),
+                          border: Border.all(color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black12.withOpacity(0.05),
+                                spreadRadius: 1,
+                                blurRadius: 7,
+                                offset: const Offset(0, -1)),
+                          ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 30,
-                        ),
-                        child: Container(
-                          height: size.height * 0.08,
-                          width: size.width,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: backgroundColor3.withOpacity(0.9),
-                            border: Border.all(color: Colors.white),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.black12.withOpacity(0.05),
-                                  spreadRadius: 1,
-                                  blurRadius: 7,
-                                  offset: const Offset(0, -1)),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 5),
+                          child: Row(
+                            children: [
+                              Container(
+                                height: size.height * 0.08,
+                                width: size.width / 2.2,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    'Register',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                              const Spacer(),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const SignIn()));
+                                },
+                                child: Text(
+                                  'Sign In',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: textColor1),
+                                ),
+                              ),
+                              const Spacer(),
                             ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ))
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
