2d4412d0c6629b6c48109742793d1eed0e898cb9
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-10 09:39:32 +0300

    git ignore files

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:login_page_flutter/pages/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Authetication Page', home: LoginScreen());
+  }
+}
