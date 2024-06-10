afd3f806ac8a4e9c86e81af50322f0a11fa8d848
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-10 12:25:32 +0300

    initial commit

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:login_page_flutter/utils/colors.dart';
 
@@ -22,14 +20,69 @@ class SplashScreen extends StatelessWidget {
                 height: size.height * 0.5,
                 width: size.width,
                 decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40)),
                   color: primaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(100),
-                    bottomRight: Radius.circular(100),
-                  ),
+                  //image: DecorationImage(image: AssetImage('assetName'))
                 ),
               ),
-            )
+            ),
+            Positioned(
+                top: size.height * 0.6,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Discover Your \nDream Job!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            color: textColor1,
+                            height: 1.2),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Explore the most exciting jobs roles\n based on your interest and ares of study',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          //fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: textColor2,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                        ),
+                        child: Container(
+                          height: size.height * 0.08,
+                          width: size.width,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: backgroundColor3.withOpacity(0.9),
+                            border: Border.all(color: Colors.white),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black12.withOpacity(0.05),
+                                  spreadRadius: 1,
+                                  blurRadius: 7,
+                                  offset: const Offset(0, -1)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
