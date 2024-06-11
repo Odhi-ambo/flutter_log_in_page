ff112f2b02af0e83717baa911062c7f7b0b9c09c
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-11 09:56:48 +0300

    initial commit

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -48,8 +48,110 @@ class SignIn extends StatelessWidget {
                   ),
                   textAlign: TextAlign.center,
                 ),
+                SizedBox(
+                  height: size.height * 0.04,
+                ),
+                myTextField("Enter username", Colors.white),
+                const SizedBox(
+                  height: 10,
+                ),
+                myTextField("Password", Colors.black26),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the value as needed
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Password Recovery",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: textColor2),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.04,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: size.width,
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        decoration: BoxDecoration(
+                          color: buttonColor,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: size.height * 0.06,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 2,
+                            width: size.width * 0.2,
+                            color: Colors.black12,
+                          ),
+                          Text(
+                            '  Or continue with  ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: textColor2),
+                          ),
+                          Container(
+                            height: 2,
+                            width: size.width * 0.2,
+                            color: Colors.black12,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container myTextField(String hint, Color color) {
+    return Container(
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          hintText: hint,
+          hintStyle: const TextStyle(color: Colors.black, fontSize: 19),
+          suffixIcon: Icon(
+            Icons.visibility_off_outlined,
+            color: color,
           ),
         ),
       ),
